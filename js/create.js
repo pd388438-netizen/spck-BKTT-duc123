3e370eb980f331b2f347706611042fe2ab959f6c
Duc[C <pd388438@gmail.com>
2025-12-21 11:26:24 +0700

    ggg

--- js/create.js
+++ js/create.js
@@ -1,4 +1,30 @@
 const btnCreate = document.getElementById("btn-create");
 btnCreate.addEventListener("click", function () {
-  alert("Create button clicked!");
+  const tieuDe = document.getElementById("tieu-de").value;
+  const img = document.getElementById("anh").value;
+  const noiDung = document.getElementById("content").value;
+  if (!tieuDe) {
+    alert("Vui lòng nhập tiêu đề bài viết");
+    return;
+  }
+  if (!img) {
+    alert("Vui lòng nhập đường link ảnh");
+    return;
+  }
+  if (!noiDung) {
+    alert("Vui lòng nhập nội dung bài viết");
+    return;
+  }
+
+  const post = {
+    id: Date.now(),
+    title: tieuDe,
+    img: img,
+    content: noiDung,
+  };
+
+  const posts = JSON.parse(localStorage.getItem("posts")) || [];
+  posts.push(post);
+  localStorage.setItem("posts", JSON.stringify(posts));
+  window.location.href = "../html/index.html";
 });
