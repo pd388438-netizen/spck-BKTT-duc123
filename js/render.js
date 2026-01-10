4ee10afea8aecfb6083d6aa2f9a7a2b5f63516b6
Duc[C <pd388438@gmail.com>
2026-01-10 17:27:30 +0700

    af da

--- js/render.js
+++ js/render.js
@@ -8,6 +8,14 @@ let html = ``;
 posts.forEach((post) => {
   html += `
     <div id="post" class="border border-gray-200 shadow-sm p-3">
+    <div class="bg-purple-200 flex h-8 items-center gap-3 font-bold">
+        <div
+          class="bg-purple-400 h-full flex items-center justify-center w-8 text-white"
+        >
+          <i class="fa-solid fa-star"></i>
+        </div>
+        <h1 class="h1">Bài viết mới</h1>
+      </div>
         <div id="post-title" class="text-xl"><h1><a href="../html/baiviet1.html?${post.id}">${post.title}</a></h1></div>
         <div class="flex items-center gap-5">
           <div id="post-content" class="">
